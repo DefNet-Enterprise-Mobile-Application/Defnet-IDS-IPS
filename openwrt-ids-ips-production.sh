2b5dc6b73f6564d629a5339677ea580b0e2ae25c
Umberto Della Monica <umberto.dellamonica@hotmail.it>
2025-02-04 23:33:00 +0100

    Update openwrt-ids-ips-production.sh

--- openwrt-ids-ips-production.sh
+++ openwrt-ids-ips-production.sh
@@ -6,7 +6,7 @@ LOG_FILE="/tmp/openwrt-ids-ips.log"  # File di log
 SERVICE_PID_FILE="/tmp/openwrt-ids-ips.pid"  # File per memorizzare il PID
 
 # Imposta un'interfaccia di rete predefinita (modifica se necessario)
-DEFAULT_INTERFACE="br-lan"
+DEFAULT_INTERFACE="phy0-sta0"
 
 # Funzione per gestire gli argomenti passati allo script
 get_interface() {
